fa8d6b2296291cdaf87d2b46607940a4f61e9c5f
ChristianSm1t <chrsmit@web.de>
2023-03-18 17:35:19 +0100

    create handleSelectAll

--- src/components/Checkbox/CheckboxGroup.tsx
+++ src/components/Checkbox/CheckboxGroup.tsx
@@ -1,5 +1,42 @@
+import React, { useState } from 'react'
+import { type CheckboxData } from '../../types/checkbox'
+
+const checkboxes: CheckboxData[] = [
+  { id: '1', name: 'hat' },
+  { id: '2', name: 'pullover' },
+  { id: '3', name: 'trowsers' },
+]
+
 function CheckboxGroup() {
-  return <div>CheckboxGroup</div>
+  const [selectAll, setSelectAll] = useState<boolean>(false)
+  const [selected, setSelected] = useState<string[]>([])
+
+  function handleSelectAll(e: React.ChangeEvent<HTMLInputElement>): void {
+    setSelectAll(e.target.checked)
+    setSelected(
+      e.target.checked ? checkboxes.map((checkbox) => checkbox.id) : []
+    )
+  }
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          handleSelectAll(e)
+        }}
+      />
+      {checkboxes.map((checkbox) => {
+        return (
+          <input
+            key={checkbox.id}
+            type="checkbox"
+            checked={selected.includes(checkbox.id)}
+          />
+        )
+      })}
+    </>
+  )
 }
 
 export default CheckboxGroup
---
+++ src/types/checkbox.ts
@@ -0,0 +1,4 @@
+export interface CheckboxData {
+  id: string
+  name: string
+}
