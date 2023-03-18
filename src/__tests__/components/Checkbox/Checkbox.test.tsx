9ac15fecd9414a1f97abb3f3b45d77e793b34427
ChristianSm1t <chrsmit@web.de>
2023-03-18 20:12:00 +0100

    tests for Checkbox component

--- src/__tests__/components/Checkbox/Checkbox.test.tsx
+++ src/__tests__/components/Checkbox/Checkbox.test.tsx
@@ -1,5 +1,5 @@
 import { describe, vi, it } from 'vitest'
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { type CheckboxProps } from '../../../types/checkbox'
 import Checkbox from '../../../components/Checkbox/Checkbox'
 
@@ -14,6 +14,38 @@ describe('one Checkbox', () => {
 
   it('Renders a Checkbox with Label', () => {
     const wrapper = render(<Checkbox {...checkBoxProps} />)
+    const checkbox: HTMLInputElement = screen.getByLabelText(checkBoxProps.name)
+    fireEvent.click(checkbox)
+
+    expect(checkbox).toBeInTheDocument()
+    expect(checkbox.getAttribute('id')).toBe(checkBoxProps.id)
+    expect(checkbox.getAttribute('type')).toBe('checkbox')
+    expect(checkbox.checked).toBe(checkBoxProps.isChecked)
+    expect(onChange).toHaveBeenCalled(1)
+
+    wrapper.unmount()
+  })
+  it('Renders unchecked checkbox when isChecked is false', () => {
+    const falseCheckboxProps: CheckboxProps = {
+      ...checkBoxProps,
+      isChecked: false,
+    }
+    const wrapper = render(<Checkbox {...falseCheckboxProps} />)
+    const checkbox: HTMLInputElement = screen.getByLabelText(checkBoxProps.name)
+
+    expect(checkbox.checked).toBe(false)
+
+    wrapper.unmount()
+  })
+  it('Renders checked checkbox when isChecked is true', () => {
+    const trueCheckboxProps: CheckboxProps = {
+      ...checkBoxProps,
+      isChecked: true,
+    }
+    const wrapper = render(<Checkbox {...trueCheckboxProps} />)
+    const checkbox: HTMLInputElement = screen.getByLabelText(checkBoxProps.name)
+
+    expect(checkbox.checked).toBe(true)
 
     wrapper.unmount()
   })
